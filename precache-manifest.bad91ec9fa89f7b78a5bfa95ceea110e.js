794196dc5270ea58740a6ba99c1b0f0b508f97c9
dflexcee <marvellousubani@gmail.com>
2019-11-21 20:09:55 +0100

    Updates

---
+++ precache-manifest.bad91ec9fa89f7b78a5bfa95ceea110e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b40e68ac8de3a5792fbd6eb50bd8c69f",
+    "url": "/employees-list/index.html"
+  },
+  {
+    "revision": "aebaf41065185c8613d3",
+    "url": "/employees-list/static/css/main.e19c2de3.chunk.css"
+  },
+  {
+    "revision": "0423b25d3f914726fee7",
+    "url": "/employees-list/static/js/2.3536e21f.chunk.js"
+  },
+  {
+    "revision": "aebaf41065185c8613d3",
+    "url": "/employees-list/static/js/main.cff94021.chunk.js"
+  },
+  {
+    "revision": "90e8e5e40d7eaf08057f",
+    "url": "/employees-list/static/js/runtime-main.d993fdb2.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/employees-list/static/media/logo.25bf045c.svg"
+  }
+]);
